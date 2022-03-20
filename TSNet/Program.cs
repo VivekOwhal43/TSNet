0d3ac6c2e0e82c4a779ba76d851ea62dc93891b8
VivekOwhal43 <vivek123.co@gmail.com>
2022-03-20 17:35:17 +0530

    [Vivek]Added Student class with a constructor for student information

--- TSNet/Program.cs
+++ TSNet/Program.cs
@@ -11,16 +11,37 @@ namespace TSNet // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Human human = new();
-            human.canSpeak();
-            //human.canSpeak();
-            StarPattern pattern = new StarPattern();
-            pattern.printStar();
+            Console.WriteLine("Enter your choice from below");
+            Console.WriteLine("\n1.Call Human class \n2.Call Star Pattern method \n3.Find number is even or odd \n4.Student information using constructor.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("You are in case 1");
+                    Human human = new();
+                    human.canSpeak();
+                    break;
 
-            EvenOdd evenodd = new();
-            evenodd.Even_odd();
-            
+                case 2:
+                    Console.WriteLine("You are in case 2");
+                    StarPattern pattern = new StarPattern();
+                    pattern.printStar();
+                    break;
+
+               case 3:
+                    EvenOdd evenodd = new EvenOdd();
+                    evenodd.Even_odd();
+                    break;
+
+                case 4:
+                    Student student = new Student(5, "Vivek");
+                    student.showDetails();
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter a number between 1 to 4");
+                break;
+            }
         }
     }
 }
---
+++ TSNet/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSNet
+{
+    internal class Student
+    {
+        string name;
+        int roll_number;
+        public Student(int roll_number, string name)
+        {
+            this.roll_number = roll_number;
+            this.name = name;
+        }
+
+        public void showDetails()
+        {
+            Console.WriteLine("Roll Number"+roll_number+" Name: "+name);
+        }
+    }
+}

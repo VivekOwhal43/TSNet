7d90d04fb6d4e4954e76db9ae1c9989cbd39ec56
VivekOwhal43 <vivek123.co@gmail.com>
2022-03-20 11:49:35 +0530

    [Vivek] Star Printing Pattern added

---
+++ TSNet/Human.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSNet
+{
+    internal class Human
+    {
+        public void canSpeak()
+        {
+            Console.WriteLine("Human Can speak");
+        }
+    }
+}
